f82adf97c58d50cd6cc298ddd1808b3f04610868
Adam Farizi <adamfarizi4@gmail.com>
2024-08-20 13:59:02 +0700

    update error page

---
+++ resources/views/error/404.blade.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Al-Huda</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/pondok/logo.png') }}">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <!-- Typography CSS -->
+    <link rel="stylesheet" href="{{ asset('css/typography.css') }}">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
+    <link
+        href="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-1.13.8/b-2.4.2/b-colvis-2.4.2/b-html5-2.4.2/b-print-2.4.2/cr-1.7.0/date-1.5.1/fc-4.3.0/fh-3.4.0/kt-2.11.0/r-2.5.0/rr-1.4.1/sc-2.3.0/sb-1.6.0/datatables.min.css"
+        rel="stylesheet">
+
+    <style>
+        a {
+            text-decoration: none;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Wrapper Start -->
+    <div class="wrapper">
+        <div class="text-center">
+            <img src="{{ asset('images/local/404.png') }}" width="450px" class="img-fluid" alt="Page Not Found">
+            <h3 class="">Oops! Sepertinya Anda telah tersesat</h3>
+            <h4 class="mb-5">Halaman yang Anda cari tidak ada di sini</h4>
+            <a type="button" href="javascript:history.back()" class="btn btn-primary">Kembali</a>
+            <br>
+        </div>
+        <div class="mt-5 text-center">
+            &copy; Copyright <strong><span>Al-Huda</span></strong>. All Rights Reserved
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <!-- Appear JavaScript -->
+    <script src="{{ asset('js/jquery.appear.js') }}"></script>
+    <!-- Countdown JavaScript -->
+    <script src="{{ asset('js/countdown.min.js') }}"></script>
+    <!-- Counterup JavaScript -->
+    <script src="{{ asset('js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
+    <!-- Wow JavaScript -->
+    <script src="{{ asset('js/wow.min.js') }}"></script>
+    <!-- Apexcharts JavaScript -->
+    <script src="{{ asset('js/apexcharts.js') }}"></script>
+    <!-- Slick JavaScript -->
+    <script src="{{ asset('js/slick.min.js') }}"></script>
+    <!-- Select2 JavaScript -->
+    <script src="{{ asset('js/select2.min.js') }}"></script>
+    <!-- Owl Carousel JavaScript -->
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+    <!-- Magnific Popup JavaScript -->
+    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <!-- Smooth Scrollbar JavaScript -->
+    <script src="{{ asset('js/smooth-scrollbar.js') }}"></script>
+    <!-- lottie JavaScript -->
+    <script src="{{ asset('js/lottie.js') }}"></script>
+    <!-- Chart Custom JavaScript -->
+    <script src="{{ asset('js/chart-custom.js') }}"></script>
+    <!-- Custom JavaScript -->
+    <script src="{{ asset('js/custom.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- DataTables -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script
+        src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-1.13.8/b-2.4.2/b-colvis-2.4.2/b-html5-2.4.2/b-print-2.4.2/cr-1.7.0/date-1.5.1/fc-4.3.0/fh-3.4.0/kt-2.11.0/r-2.5.0/rr-1.4.1/sc-2.3.0/sb-1.6.0/datatables.min.js">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.4/xlsx.full.min.js"></script>
+</body>
+
+</html>
